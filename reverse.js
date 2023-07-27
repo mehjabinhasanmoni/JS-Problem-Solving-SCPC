1a2bd05338a3651170b5720baf44711a54d13dc7
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-07-27 17:05:39 +0600

    Reverse string problem solving with out using reverse function

--- reverse.js
+++ reverse.js
@@ -1,9 +1,14 @@
 // Problem : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 
 function reverseString (str) {
-    let reverseStr = str.split("").reverse().join("");
+    let reverseStr = "";
+
+    for (let i= str.length -1; i>=0 ; i--) {
+        reverseStr+= str[i];
+    }
+
     return reverseStr;
 }
 
-let strr = reverseString("Hello World")
-console.log(strr);
+let inputStr = reverseString("Hello World")
+console.log(inputStr);
